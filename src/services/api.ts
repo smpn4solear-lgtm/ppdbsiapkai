73c7cdcaf41110a45d134c30fc586d6cde0a9763
smpn4solear-lgtm <smpn4solear@gmail.com>
2026-05-26 15:51:03 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxBUIcxb6mmfgyAcPIctHOa6u-dN2NmGFUpN2HA6J3710D2F4rcUZ3dHrJzP4MGkows/exec"; 
 
 export interface FormField {
   id: string;
